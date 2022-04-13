cc49b4551347cf6b322fd56229af340b3b14bf12
Rovin Dore <rovindore@hotmail.com>
2022-04-13 15:02:53 -0400

    Form Update

--- VehicleRegistrationForm/js/index.js
+++ VehicleRegistrationForm/js/index.js
@@ -1,11 +1,13 @@
 function showtext() {
-
     var brand = document.getElementById('brand').value;
     var color = document.getElementById('color').value;
     var type = document.getElementById('vehicleType').value;
     var model = document.getElementById('model').value;
 
     document.getElementById("show").innerHTML = "Brand: " + brand + "<br>" + "Color: " + color + "<br>" + "Type: " + type + "<br>" + "Model: " + model;
-
-
 }
+
+document.getElementById("#RegistrationForm").addEventListener("submit", function(event){
+    showtext();
+    event.preventDefault();
+});
