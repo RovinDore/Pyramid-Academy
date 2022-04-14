f5b4e688765978d7b7c5bdf5887dc199def13629
Rovin Dore <rovindore@hotmail.com>
2022-04-14 11:04:35 -0400

    My Developer portfolio

---
+++ MyDevPortfolio/build/precache-manifest.3913a210df1324dbeca8d36471470229.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfaa3dbc4cccc83ca86ae2fde58dd54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "809b97b7e263cb456efb",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e6de53f9c98e6a6f01cb",
+    "url": "/static/css/main.121ac491.chunk.css"
+  },
+  {
+    "revision": "809b97b7e263cb456efb",
+    "url": "/static/js/2.bf38f139.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.bf38f139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6de53f9c98e6a6f01cb",
+    "url": "/static/js/main.6a946c68.chunk.js"
+  },
+  {
+    "revision": "d7971d945ca83241ca6a",
+    "url": "/static/js/runtime-main.5c012205.js"
+  },
+  {
+    "revision": "1e23eab2d353e8b7f7f6610bf5172d33",
+    "url": "/static/media/coldfusion.1e23eab2.svg"
+  }
+]);
